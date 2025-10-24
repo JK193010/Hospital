ec1c7202bd1a9a1e8002e4c5baa4b24db328e823
K1mutai <167431375+K1mutai@users.noreply.github.com>
2025-10-24 12:21:47 +0300

    commited finalvendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jayku/hospital-system-project',
+        'pretty_version' => 'dev-login-page',
+        'version' => 'dev-login-page',
+        'reference' => '55824861aba740f049d0d40bd990a15ce25e5ef5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jayku/hospital-system-project' => array(
+            'pretty_version' => 'dev-login-page',
+            'version' => 'dev-login-page',
+            'reference' => '55824861aba740f049d0d40bd990a15ce25e5ef5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
